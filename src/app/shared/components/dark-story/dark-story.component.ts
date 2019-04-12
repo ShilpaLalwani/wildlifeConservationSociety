c9220702798507579aa183719e4b93f430b87407
Shilpa Lalwani <shilpal.2507@gmail.com>
2019-04-13 00:52:35 +0530

    first commit

---
+++ src/app/shared/components/dark-story/dark-story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-story',
+  templateUrl: './dark-story.component.html',
+  styleUrls: ['./dark-story.component.css']
+})
+export class DarkStoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/components/light-story/light-story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-light-story',
+  templateUrl: './light-story.component.html',
+  styleUrls: ['./light-story.component.css']
+})
+export class LightStoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
